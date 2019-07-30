45e7b51f2cd9137c0059981c948cf008e918eb61
Maksym <boycovm37@gmail.com>
2019-07-30 21:29:00 +0200

    Updates

---
+++ precache-manifest.aabfbf68df8c849ea738fa1b527dc97f.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d1156911b27b412c78826a205d1447",
+    "url": "/alabena/index.html"
+  },
+  {
+    "revision": "1d0a1e7a31adfb605bbc",
+    "url": "/alabena/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "bc845aec6251103c2433",
+    "url": "/alabena/static/css/main.ec6a6ce3.chunk.css"
+  },
+  {
+    "revision": "1d0a1e7a31adfb605bbc",
+    "url": "/alabena/static/js/2.65b7efcf.chunk.js"
+  },
+  {
+    "revision": "bc845aec6251103c2433",
+    "url": "/alabena/static/js/main.b7a06e27.chunk.js"
+  },
+  {
+    "revision": "b2fecdd87888303e5593",
+    "url": "/alabena/static/js/runtime~main.7fee275b.js"
+  },
+  {
+    "revision": "239fdb7fd789d00161e6200977e32a77",
+    "url": "/alabena/static/media/acids.239fdb7f.png"
+  },
+  {
+    "revision": "d348d5efb0e56f20c967182fe084de66",
+    "url": "/alabena/static/media/alabena-logo-color.d348d5ef.svg"
+  },
+  {
+    "revision": "18750aaeb5b36e846eb6dda9c4ce0f88",
+    "url": "/alabena/static/media/animal-fat.18750aae.png"
+  },
+  {
+    "revision": "a01fa4277c74ec8a65a06a82450e7f1f",
+    "url": "/alabena/static/media/bg-main.a01fa427.png"
+  },
+  {
+    "revision": "1de52d556425206185979f8c94b498a8",
+    "url": "/alabena/static/media/biodiesel.1de52d55.png"
+  },
+  {
+    "revision": "cd6c7f83e4a6776ec040f68c1aea5fdb",
+    "url": "/alabena/static/media/by-prod.cd6c7f83.png"
+  },
+  {
+    "revision": "5aaed8571ca0d2d277c5e88bd8f2aa1f",
+    "url": "/alabena/static/media/cargo-ship.5aaed857.png"
+  },
+  {
+    "revision": "1c1c127e3f56f5938083fa11ded45bf7",
+    "url": "/alabena/static/media/fish.1c1c127e.png"
+  },
+  {
+    "revision": "099a6117527a2acf55cc858ed899b5f5",
+    "url": "/alabena/static/media/inspection.099a6117.svg"
+  },
+  {
+    "revision": "b6f9cce575674fffd6b99ce5e9aec408",
+    "url": "/alabena/static/media/oil-truck.b6f9cce5.png"
+  },
+  {
+    "revision": "add9531363fe065159cfee7fd70daaf3",
+    "url": "/alabena/static/media/storage-tank.add95313.svg"
+  },
+  {
+    "revision": "6230b31ffdc408a6a3c1702d44b3783c",
+    "url": "/alabena/static/media/vegetable.6230b31f.png"
+  },
+  {
+    "revision": "a57a35965adf28ff6e36a0a0c67d29dc",
+    "url": "/alabena/static/media/worker.a57a3596.svg"
+  }
+]);

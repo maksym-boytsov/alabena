632687f5f5b4fd69574530f17a9086ee8225558e
Maksym <boycovm37@gmail.com>
2019-08-04 18:09:50 +0200

    Updates

--- precache-manifest.653bb77ce8b6fb7f214c476c6a2f59fe.js
+++ precache-manifest.653bb77ce8b6fb7f214c476c6a2f59fe.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "42948614cf21641dc0e97cc5d97d1f6d",
+    "revision": "64fac91fdd502e3c2c1091d54e324aad",
     "url": "/alabena/index.html"
   },
   {
-    "revision": "1d0a1e7a31adfb605bbc",
+    "revision": "b81aeacb9bea43cdace3",
     "url": "/alabena/static/css/2.510409cd.chunk.css"
   },
   {
-    "revision": "aa0c6c1b161d410baeb8",
-    "url": "/alabena/static/css/main.c042b576.chunk.css"
+    "revision": "cfdeb3138dfb449b74e6",
+    "url": "/alabena/static/css/main.74e5ccbd.chunk.css"
   },
   {
-    "revision": "1d0a1e7a31adfb605bbc",
-    "url": "/alabena/static/js/2.65b7efcf.chunk.js"
+    "revision": "b81aeacb9bea43cdace3",
+    "url": "/alabena/static/js/2.acaef0fa.chunk.js"
   },
   {
-    "revision": "aa0c6c1b161d410baeb8",
-    "url": "/alabena/static/js/main.758fb39a.chunk.js"
+    "revision": "cfdeb3138dfb449b74e6",
+    "url": "/alabena/static/js/main.1c2e4c75.chunk.js"
   },
   {
     "revision": "b2fecdd87888303e5593",
@@ -28,6 +28,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/alabena/static/media/acids.0b3ce1b6.svg"
   },
   {
+    "revision": "aadd9acd242223762784a17b041fc03f",
+    "url": "/alabena/static/media/acids.aadd9acd.jpeg"
+  },
+  {
     "revision": "d348d5efb0e56f20c967182fe084de66",
     "url": "/alabena/static/media/alabena-logo-color.d348d5ef.svg"
   },
@@ -36,8 +40,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/alabena/static/media/animal-fat.ba31092d.svg"
   },
   {
-    "revision": "f5b44a36f4d6e5274f78bb28a0044159",
-    "url": "/alabena/static/media/bg-main.f5b44a36.jpg"
+    "revision": "f9205dafd332b654ad840faf462c3f7b",
+    "url": "/alabena/static/media/animal.f9205daf.jpeg"
+  },
+  {
+    "revision": "0420f789a8b1895e1e6d1da76632f421",
+    "url": "/alabena/static/media/bg-main.0420f789.jpg"
+  },
+  {
+    "revision": "3e8cd1382515bbfec56f9750e63191ca",
+    "url": "/alabena/static/media/biodiesel.3e8cd138.jpeg"
   },
   {
     "revision": "47b706d05e63cf81403ed06d7ad66106",
@@ -48,12 +60,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/alabena/static/media/by-prod.0f4d7a3f.svg"
   },
   {
+    "revision": "48d72bc9ed7f4361bf1eccee22ea3f1c",
+    "url": "/alabena/static/media/by_prod.48d72bc9.jpeg"
+  },
+  {
     "revision": "376b0ee4dcafc8476136112593fb0ff4",
     "url": "/alabena/static/media/cargo-ship.376b0ee4.png"
   },
   {
     "revision": "24987c7b3da549f090b6f2c1cf80805c",
     "url": "/alabena/static/media/fish-fat.24987c7b.svg"
+  },
+  {
+    "revision": "657d98176b53bec15b8aabe747907251",
+    "url": "/alabena/static/media/fish.657d9817.jpeg"
   },
   {
     "revision": "adb6ae0efc5531a8f49ca782d55d33e9",
@@ -70,6 +90,14 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "b756bd98b60ba23785e8a60a76f2694a",
     "url": "/alabena/static/media/vegetable.b756bd98.svg"
+  },
+  {
+    "revision": "8e8d4c2090f4a6f5e7744144e2f7943c",
+    "url": "/alabena/static/media/vegetable_oil.8e8d4c20.jpeg"
+  },
+  {
+    "revision": "f21101bfe7b8387e337db01896935d5b",
+    "url": "/alabena/static/media/wave.f21101bf.png"
   },
   {
     "revision": "a5df8bcd76f7d9df622b7c20f6741a80",

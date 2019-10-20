9b11632f5edc55a26d4c275806261d7d7dc7054f
Maksym Boytsov <boycovm37@gmail.com>
2019-10-20 15:59:16 +0200

    Updates

---
+++ precache-manifest.af0c3160c367f8d44730455f6375393c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4369a7331c1c9685fe4e4a66c794e372",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c571b13e5c619c76ac02",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "d8e418100866c7ee6b13",
+    "url": "/static/css/main.1cf88b2c.chunk.css"
+  },
+  {
+    "revision": "c571b13e5c619c76ac02",
+    "url": "/static/js/2.3504f71c.chunk.js"
+  },
+  {
+    "revision": "d8e418100866c7ee6b13",
+    "url": "/static/js/main.2a3ab6e5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0b3ce1b6af604398ea8a70d52a32c94c",
+    "url": "/static/media/acids.0b3ce1b6.svg"
+  },
+  {
+    "revision": "4fced5140a7f616640f91ff3e85d2175",
+    "url": "/static/media/acids.4fced514.jpg"
+  },
+  {
+    "revision": "d348d5efb0e56f20c967182fe084de66",
+    "url": "/static/media/alabena-logo-color.d348d5ef.svg"
+  },
+  {
+    "revision": "ba31092d0408ea4185c7debcff5f398b",
+    "url": "/static/media/animal-fat.ba31092d.svg"
+  },
+  {
+    "revision": "feb88001196e2fcf5945e933a70ed8b5",
+    "url": "/static/media/animal.feb88001.jpg"
+  },
+  {
+    "revision": "557029e7efede04c13a4a3dc6e72147e",
+    "url": "/static/media/bg-main.557029e7.jpg"
+  },
+  {
+    "revision": "47b706d05e63cf81403ed06d7ad66106",
+    "url": "/static/media/biodiesel.47b706d0.svg"
+  },
+  {
+    "revision": "8c9a415e1181e525f65489ec044581c1",
+    "url": "/static/media/biodiesel.8c9a415e.jpg"
+  },
+  {
+    "revision": "0f4d7a3fdcd5c1f2eefd766f085e6b77",
+    "url": "/static/media/by-prod.0f4d7a3f.svg"
+  },
+  {
+    "revision": "11e37c1dd3499727bcac08465fa31071",
+    "url": "/static/media/by_prod.11e37c1d.jpg"
+  },
+  {
+    "revision": "376b0ee4dcafc8476136112593fb0ff4",
+    "url": "/static/media/cargo-ship.376b0ee4.png"
+  },
+  {
+    "revision": "b7714116cace7edcc895c582c45ff4ae",
+    "url": "/static/media/certificate.b7714116.jpg"
+  },
+  {
+    "revision": "24987c7b3da549f090b6f2c1cf80805c",
+    "url": "/static/media/fish-fat.24987c7b.svg"
+  },
+  {
+    "revision": "b3d69abdb69529dfc2383cc8e28133ba",
+    "url": "/static/media/fish.b3d69abd.jpg"
+  },
+  {
+    "revision": "adb6ae0efc5531a8f49ca782d55d33e9",
+    "url": "/static/media/inspection.adb6ae0e.svg"
+  },
+  {
+    "revision": "e82bc9e030e053f086909708e8b38cc8",
+    "url": "/static/media/oil-truck.e82bc9e0.png"
+  },
+  {
+    "revision": "cf442b162d536ba6d4db51de64822cb3",
+    "url": "/static/media/storage-tank.cf442b16.svg"
+  },
+  {
+    "revision": "b756bd98b60ba23785e8a60a76f2694a",
+    "url": "/static/media/vegetable.b756bd98.svg"
+  },
+  {
+    "revision": "5aae3e0f47bf44c4a20ab235f900250b",
+    "url": "/static/media/vegetable_oil.5aae3e0f.jpg"
+  },
+  {
+    "revision": "a5df8bcd76f7d9df622b7c20f6741a80",
+    "url": "/static/media/worker.a5df8bcd.svg"
+  }
+]);
